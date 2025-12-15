545aa64af5baa8361b8ce4579bdd5fcaad64e05e
Igor Kriasnik <igor.kraisnik@gmial.com>
2025-12-15 10:21:03 +0100

    UX/UI Refactoring 1.6

--- web/components/PrdView.tsx
+++ web/components/PrdView.tsx
@@ -312,7 +312,7 @@ export default function PrdView({
 
       {/* Download view */}
       {viewMode === "download" && (
-        <div className="space-y-6">
+        <div className="space-y-6 overflow-y-auto flex-1 min-h-0">
           <DownloadResults
             jobId={jobId}
             markdownFilename={markdownFilename}
